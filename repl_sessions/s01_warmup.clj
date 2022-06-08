9813ae73ee66cd303e57c4a7b95f8c63bb29722d
Arne Brasseur <arne@arnebrasseur.net>
2022-06-08 08:49:10 +0200

    Add a bit to the intro, and switch back to seed 34

--- repl_sessions/s01_warmup.clj
+++ repl_sessions/s01_warmup.clj
@@ -1,11 +1,41 @@
 (ns s01-warmup
   "Some fun things to help you get a taste of Witchcraft."
-  (:require [lambdaisland.witchcraft :as wc]))
+  (:require [lambdaisland.witchcraft :as wc]
+            [lambdaisland.witchcraft.markup :as markup]))
+
+;; Welcome to the Witchcraft workshop! Hopefully you managed to get your server
+;; running, and managed to connect to it, both from the game (bin/start-client,
+;; then connect to localhost:25565), and with your editor where you are reading
+;; this, using nREPL (connect to localhost:25555).
+
+;; If so then go ahead and start going through this file, this is an introduction
+;; you can go through by yourself while we go around and make sure everyone has a
+;; working setup. Once everyone is up and running we'll move on to part two.
+
+;; If all went well you are standing on some grass with some birch and oak trees
+;; nearby. Have a look around! You can move around with the mouse + w/a/s/d
+;; keys. Try breaking a block by holding the left mouse button. Good job, you're
+;; playing minecraft.
+
+;; We want to manipulate this world you find yourself in, through a Clojure REPL.
+;; But it's a big world, and we like to be able to see the changes we make, so
+;; let's figure out where we are in the world first.
 
 ;; If there's only one player online then this will get you that player, or pass
 ;; it a player name to be explicit
 
-(def me (wc/player))
+(def me (wc/player #_"sunnyplexus"))
+
+(wc/send-title me (markup/fAnCy "Hello from Clojure!" [:dark-blue :dark-green]))
+
+;; `loc` will give you the location of something as a Clojure mapcat
+
+(wc/loc me)
+
+(wc/send-title me (pr-str (wc/locv me)))
+
+;; (You can also try `locv`, `block`, `get-block`, `target-block`, `x`, `y`,
+;; `z`, `inventory`)
 
 ;; Let's save our current location, so we have a point of reference
 (defonce anchor (wc/loc me))
@@ -38,23 +68,8 @@
 ;; Now pressing SPACE will move you upwards, and SHIFT will get you down. Once
 ;; you've landed press SPACE twice to fly again.
 
-;; Alternatively you can give yourself wings, and propel yourself with firework rockets
+;; Let's try some other things, let's spawn some chickens!
 
-;; Let's turn that off again first
-(wc/allow-flight me false)
-
-;; Add these
-(wc/add-inventory (wc/player) :elytra 1)
-(wc/add-inventory (wc/player) :firework-rocket 64)
-
-;; Open your inventory (`e`), and move the wings to the chestplate armor
-;; slot (top left next to your avatar). Close your inventory again, and make
-;; sure you have the rockets in your hand (mouse scroll wheel or number keys
-;; 1-9).
-;;
-;; Now press SPACE twice and click right. Free like a bird. Be careful landing!
-
-;; Let's spawn some chickens!
 (def chickens
   (doall
    (repeatedly 10 #(wc/spawn (wc/add anchor [0 4 0]) :chicken))))
@@ -65,3 +80,29 @@
   ;; Witchcraft will convert `chicken` to a location
   (wc/create-explosion chicken 5.0)
   (Thread/sleep 500))
+
+;; Let's give ourselves some gear: a pickaxe with silk touch, some protection,
+;; and some food to munch on.
+
+(wc/into-inventory
+ me
+ [{:material :diamond-pickaxe
+   :enchants {:silk-touch 1}
+   :lore [[:<>
+           [:gold "A "]
+           [:white "gossamer "]
+           [:gold "pick"]]]}
+
+  {:material :netherite-chestplate
+   :enchants {:protection 5
+              :blast-protection 5}}
+
+  {:material :netherite-boots
+   :enchants {:feather-falling 5}}
+
+  [:golden-carrot 64]])
+
+;; You can jump around the server a bit to see different landscapes and biomes.
+;; Note that this can put a lot of strain on your CPU (and battery), since it
+;; needs to generate all that new landscape.
+(wc/teleport me [(rand-int 10000) 100 (rand-int 10000)])
